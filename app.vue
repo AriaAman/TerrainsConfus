201a2c64a282cb9201c0d9cc0f2292927941674e
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-04-20 17:19:56 +0200

    add nuxtpage to app

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@ useHead({
   bodyAttrs: {
     class: 'aménagement-extérieur'
   },
-  script: [ { innerHTML: 'console.log(\'Bienvenue sur Terrain Confus\')' } ]
+  script: [{ innerHTML: 'console.log(\'Bienvenue sur Terrain Confus\')' }]
 })
 
 useSeoMeta({
@@ -27,9 +27,10 @@ useSeoMeta({
 
 <template>
   <div>
-    <UI-head/>
-    <Home/>
-    <UI-foot/>
+    <UI-head />
+    <!-- <Home/> -->
+    <NuxtPage page-key="static" />
+    <UI-foot />
   </div>
 </template>
 
@@ -39,44 +40,76 @@ useSeoMeta({
 * {
   font-family: "Poppins", sans-serif;
 }
-h6, .h6, h5, .h5, h4, .h4, h3, .h3, h2, .h2, h1, .h1 {
-    margin-top: 0;
-    margin-bottom: 0.5rem;
-    font-weight: 500;
-    line-height: 1.2;
+
+h6,
+.h6,
+h5,
+.h5,
+h4,
+.h4,
+h3,
+.h3,
+h2,
+.h2,
+h1,
+.h1 {
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  line-height: 1.2;
 }
-h3{
+
+h3 {
   display: block;
   margin-block-start: 1em;
   margin-block-end: 1em;
   margin-inline-start: 0px;
   margin-inline-end: 0px;
 }
-.container, .container-fluid, .container-xxl, .container-xl, .container-lg, .container-md, .container-sm {
-    --bs-gutter-x: 1.5rem;
-    --bs-gutter-y: 0;
-    width: 100%;
-    padding-right: calc(var(--bs-gutter-x) * 0.5);
-    padding-left: calc(var(--bs-gutter-x) * 0.5);
-    margin-right: auto;
-    margin-left: auto;
+
+.container,
+.container-fluid,
+.container-xxl,
+.container-xl,
+.container-lg,
+.container-md,
+.container-sm {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  width: 100%;
+  padding-right: calc(var(--bs-gutter-x) * 0.5);
+  padding-left: calc(var(--bs-gutter-x) * 0.5);
+  margin-right: auto;
+  margin-left: auto;
 }
-input, button, select, optgroup, textarea {
+
+input,
+button,
+select,
+optgroup,
+textarea {
   margin: 0;
   font-family: inherit;
   font-size: inherit;
   line-height: inherit;
 }
-*, *::before, *::after {
+
+*,
+*::before,
+*::after {
   box-sizing: border-box;
 }
+
 p {
   margin-top: 0;
   margin-bottom: 1rem;
 }
-html, body {
+
+html,
+body {
   height: 100%;
 }
+
 body {
   display: flex;
   flex-direction: column;
@@ -85,17 +118,18 @@ body {
 
 /* Scrollbar Styling */
 ::-webkit-scrollbar {
-    width: 7px;
+  width: 7px;
 }
+
 ::-webkit-scrollbar-track {
-    background-color: #ebebeb;
-    -webkit-border-radius: 10px;
-    border-radius: 10px;
+  background-color: #ebebeb;
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
 }
 
 ::-webkit-scrollbar-thumb {
-    -webkit-border-radius: 10px;
-    border-radius: 10px;
-    background: #6d6d6d; 
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  background: #6d6d6d;
 }
 </style>
