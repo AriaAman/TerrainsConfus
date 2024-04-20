d2682d1c9334dfda0be9466ec9a620c092f4d465
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-04-20 15:53:45 +0200

    add robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/i18n'],
+  modules: ["@nuxtjs/i18n", "@nuxtjs/robots"],
   i18n: {
-    vueI18n: './UI/language/i18n.js',
-    locales: [ 
-                      { code: 'FR', name: 'Francais', iso: 'fr-FR', file: './components/UI/language/francais.json', },
-                      { code: 'EN', name: 'English', iso: 'en-EN', file: './components/UI/language/english.json', },
-                  ],
-    defaultLocale: 'EN',
-  }
-})
+    vueI18n: "./UI/language/i18n.js",
+    locales: [
+      {
+        code: "FR",
+        name: "Francais",
+        iso: "fr-FR",
+        file: "./components/UI/language/francais.json",
+      },
+      {
+        code: "EN",
+        name: "English",
+        iso: "en-EN",
+        file: "./components/UI/language/english.json",
+      },
+    ],
+    defaultLocale: "EN",
+  },
+});
