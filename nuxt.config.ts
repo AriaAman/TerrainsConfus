4d9319a22575b12f9614be5031bfc9204f30ae88
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-05-25 18:35:57 +0200

    add complete seo module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,17 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
       htmlAttrs: {
-        lang: 'fr',
+        lang: "fr",
       },
     },
   },
   site: {
+    name: "Le Terrain Confus",
     url: "https://leterrainconfus.fr",
   },
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/robots",
-    "@nuxtjs/sitemap",
-    "@nuxtjs/tailwindcss",
-    "@nuxt/content",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@nuxtjs/seo"],
 });
